67238d890c007bee34b47335fd674f4976282460
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2023-06-20 21:23:29 +0500

    init

---
+++ core/Router.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core;
+
+class Router
+{
+    public function get()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function run()
+    {
+
+    }
+}
