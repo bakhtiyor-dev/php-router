9389b8cbc48b81f210590fa0e5fc1666b94d6800
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2023-06-21 14:57:30 +0500

    Router class implemented

--- core/Router.php
+++ core/Router.php
@@ -4,20 +4,50 @@ declare(strict_types=1);
 
 namespace Core;
 
+use Closure;
+
 class Router
 {
-    public function get()
-    {
+    private array $handlers;
 
+    private Closure $notFoundHandler;
+
+    private const METHOD_GET = 'GET';
+
+    private const METHOD_POST = 'POST';
+
+    public function get(string $path, callable $handler)
+    {
+        $this->addHandler(self::METHOD_GET, $path, $handler);
     }
 
-    public function post()
+    public function post(string $path, callable $handler)
     {
+        $this->addHandler(self::METHOD_POST, $path, $handler);
+    }
 
+    public function addNotFoundHandler(callable $handler)
+    {
+        $this->notFoundHandler = $handler;
+    }
+
+    private function addHandler(string $method, string $path, callable $handler)
+    {
+        $this->handlers["{$method}{$path}"] = compact('method', 'path', 'handler');
     }
 
     public function run()
     {
+        $requestUri = parse_url($_SERVER['REQUEST_URI']);
+        $requestPath = $requestUri['path'];
+        $method = $_SERVER['REQUEST_METHOD'];
 
+        foreach ($this->handlers as $handler) {
+            if ($handler['path'] === $requestPath && $method === $handler['method']) {
+                return call_user_func($handler['handler']);
+            }
+        }
+
+        return call_user_func($this->notFoundHandler);
     }
 }
--- public/index.php
+++ public/index.php
@@ -7,3 +7,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Core\Router;
 
 $router = new Router();
+
+$router->get('/', function () {
+    return json_encode(['type' => 'asdsa']);
+});
+
+$router->addNotFoundHandler(fn() => 'Not Found!');
+
+echo $router->run();
